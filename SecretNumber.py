9b13b3c4ab36a0d6c6fd12977aea25e879b86f60
Zee-yan <48661843+Gyaneshp@users.noreply.github.com>
2022-11-17 14:10:14 +1100

    Update SecretNumber.py

--- SecretNumber.py
+++ SecretNumber.py
@@ -1,14 +1,14 @@
-# Think of any Secret number
+# Think of any Secret number, and I'll find out!
 import random
-multiply_number = random.randrange(1,6)
-add_number = random.randrange(5,20)
-divide_number = random.randrange(2,4)
 
-input("Think of any Secret number: (then, hit Enter)")
+multiply_number = random.randrange(2,5)
+add_number = random.randrange(2,11, 2)
+divide_number = random.randrange(2,5, 2)
+
+input("\nThink of any Secret number: (hit Enter)")
 input(f"Multiply it by {multiply_number}: (hit Enter)")
 input(f"add {add_number}: (hit Enter)")
-input(f"divide by {divide_number}: (hit Enter)")
 
-remain_number = float(input("Now, what is remain?: "))
+remain_number = float(input(f"Divide by {divide_number}, and what is remain?: "))
 secret_number = float((remain_number * divide_number - add_number) // multiply_number)
-print(f"\nYour Secret Number is: {secret_number}")
+print(f"---------------------------\nYour Secret Number is: {secret_number}\n---------------------------")
