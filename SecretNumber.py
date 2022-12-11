4ef96b5fd1455fe77512f964c8ff7f8825c818ac
Gyaneshp <gyan.charm@gmail.com>
2022-12-12 10:01:27 +1100

    added thank you.

---
+++ SecretNumber.py
@@ -0,0 +1,15 @@
+# Think of any Secret number, and I'll find out!
+import random
+
+multiply_number = random.randrange(2,5)
+add_number = random.randrange(2,11, 2)
+divide_number = random.randrange(2,5, 2)
+
+input("\nThink of any Secret number: (hit Enter)")
+input(f"Multiply it by {multiply_number}: (hit Enter)")
+input(f"add {add_number}: (hit Enter)")
+
+remain_number = float(input(f"Divide by {divide_number}, and what is remain?: "))
+secret_number = float((remain_number * divide_number - add_number) // multiply_number)
+print(f"--------------------------\nYour Secret Number is: {secret_number}\n--------------------------")
+print("Thank you for playing, please come back again!")
